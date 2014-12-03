655d2dfc9d780863cc33af7d1d9dad72be15c06e
Jesper Turesson <Jturesson90gmail.com>
2014-12-03 01:13:53 +0100

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+		public static float JUMP_VELOCITY = 45;
+		public static float MOVE_VELOCITY = 60;
+
+		// Use this for initialization
+		
+		void Start ()
+		{
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				CheckTrigger ();
+				HandleInput ();
+				HandlePlayerToBeOnScreen ();
+				
+
+		}
+
+
+		private void HandlePlayerToBeOnScreen ()
+		{
+				Vector3 positionInScreen = Camera.main.WorldToScreenPoint (transform.position);
+				if (positionInScreen.x < 0) {
+						transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (Camera.main.pixelWidth, positionInScreen.y, positionInScreen.z));
+				} else if (positionInScreen.x > Camera.main.pixelWidth) {
+						transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (0, positionInScreen.y, positionInScreen.z));
+				}
+				//transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
+		}
+
+		private void CheckTrigger ()
+		{
+				
+				if (rigidbody2D.velocity.y > 0) {
+						collider2D.isTrigger = true;
+						
+				} else {
+						collider2D.isTrigger = false;
+				}
+		}
+
+		private void HandleInput ()
+		{
+				float dirX = Input.acceleration.x * MOVE_VELOCITY * Time.deltaTime;
+				
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+						dirX = -MOVE_VELOCITY * Time.deltaTime;
+				}
+				if (Input.GetKey (KeyCode.RightArrow)) {
+						dirX = MOVE_VELOCITY * Time.deltaTime;
+				}
+				transform.Translate (dirX, 0, 0);
+		}
+
+		private void Jump ()
+		{
+				rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 0f);
+				rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, JUMP_VELOCITY);
+				audio.Play ();
+		}
+
+		void OnCollisionEnter2D (Collision2D coll)
+		{
+				print (coll.gameObject.tag);
+
+				switch (coll.gameObject.tag) {
+				case "Bounce":
+						Jump ();
+						break;
+				default :
+						break;
+				}
+		}
+}
---
+++ Assets/Scripts/SpawnPlatforms.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatforms : MonoBehaviour
+{
+
+		public GameObject[] differentPlatforms;
+		private float y = 0;
+		// Use this for initialization
+		void Awake ()
+		{
+				y = transform.position.y + PlatformScript.PLATFORM_HEIGHT;
+				for (int i = 0; i < 50; i++) {
+						SpawnPlatform ();
+				}
+		}
+		void Start ()
+		{
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				int len = GameObject.FindGameObjectsWithTag ("Bounce").Length;
+				if (len < 50) {
+						SpawnPlatform ();
+						
+				}
+		}
+		private void SpawnPlatform ()
+		{
+				float x = Camera.main.pixelWidth / 2;
+				Vector3 spawnPlatformPosition = Camera.main.ScreenToWorldPoint (new Vector3 (x, y, 10));
+			
+				spawnPlatformPosition.y = y;
+				
+				GameObject newPlatform = Instantiate (differentPlatforms [0], spawnPlatformPosition, Quaternion.identity) as GameObject;
+				newPlatform.transform.parent = gameObject.transform;
+				
+				if (ScoreManager.score < 300) {
+						y += Random.Range (PlatformScript.PLATFORM_HEIGHT, (PlayerScript.JUMP_VELOCITY * PlayerScript.JUMP_VELOCITY / (2 * -Physics2D.gravity.y) * 0.1f));
+				} else if (ScoreManager.score < 1000) {
+						y += Random.Range (PlatformScript.PLATFORM_HEIGHT, (PlayerScript.JUMP_VELOCITY * PlayerScript.JUMP_VELOCITY / (2 * -Physics2D.gravity.y) * 0.5f));
+				} else if (ScoreManager.score < 2000) {
+						y += Random.Range (PlatformScript.PLATFORM_HEIGHT * 2, PlayerScript.JUMP_VELOCITY * PlayerScript.JUMP_VELOCITY / (2 * -Physics2D.gravity.y));
+				} else if (ScoreManager.score < 4000) {
+						y += Random.Range (PlatformScript.PLATFORM_HEIGHT * 4, PlayerScript.JUMP_VELOCITY * PlayerScript.JUMP_VELOCITY / (2 * -Physics2D.gravity.y));
+				} else {
+						y += PlayerScript.JUMP_VELOCITY * PlayerScript.JUMP_VELOCITY / (2 * -Physics2D.gravity.y) - 2f;
+				}
+		}
+}
